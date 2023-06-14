4fda9f078e4600aed444f42e9eed1576d9e2bd6a
ianpknsl <ugurpekunsal@gmail.com>
2023-06-14 19:27:23 -0400

    Added experience

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,13 +49,12 @@ class Header extends Component {
 				<div className="row aligner" style={{ height: "100%" }}>
 					<div className="col-md-12">
 						<div>
-							<span
-								className="iconify header-icon"
-								data-icon="la:laptop-code"
-								data-inline="false"
-							></span>
+							<img
+								height={150}
+								src="..\..\images\portfolio\main-icon.png"
+							></img>
 							<br />
-							<h1 className="mb-0">
+							<h1 className="mb-0 text-black">
 								<Typical steps={[name]} wrapper="p" />
 							</h1>
 							<div className="title-container">
